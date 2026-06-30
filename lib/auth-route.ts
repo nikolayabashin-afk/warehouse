d7e32f0ca2ecd9a5dee6db526ead2feeb1e2394f
Nikolay <nikolay.abashin@gmail.com>
2026-06-30 17:54:58 +0200

    Remove auth handler rollback

--- lib/auth-route.ts
+++
@@ -1,7 +0,0 @@
-import NextAuth from 'next-auth'
-import { authOptions as options } from './auth'
-
-const routeHandler = NextAuth(options)
-
-export const GET = routeHandler
-export const POST = routeHandler
