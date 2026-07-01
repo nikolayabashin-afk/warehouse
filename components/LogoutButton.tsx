78b8965fc83b90fdc5f0d511a49779fa1e988fe3
Nikolay <nikolay.abashin@gmail.com>
2026-07-01 09:31:50 +0200

    Add logout button

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { signOut } from 'next-auth/react'
+
+export function LogoutButton() {
+  return (
+    <button
+      type="button"
+      className="btn-secondary whitespace-nowrap"
+      onClick={() => signOut({ callbackUrl: '/login' })}
+    >
+      Выйти
+    </button>
+  )
+}
